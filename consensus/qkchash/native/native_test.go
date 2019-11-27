f8d0e6ec5aaf6ca4c3964839fe3b1c6550559d3f
TAO <1659791130@qq.com>
2019-11-27 10:32:42 +0800

    update

--- consensus/qkchash/native/native_test.go
+++ consensus/qkchash/native/native_test.go
@@ -34,5 +34,5 @@ func TestNativeTokenTransfer(t *testing.T) {
 	b1, _ := shardState.CreateBlockToMine(nil, &acc3, nil, nil, nil)
 	assert.Equal(t, len(b1.Transactions()), 1)
 	shardState.FinalizeAndAddBlock(b1)
-	assert.Equal(t, shardState.CurrentBlock().Header(), b1.Header())
+	assert.Equal(t, shardState.CurrentHeader(), b1.Header())
 }
