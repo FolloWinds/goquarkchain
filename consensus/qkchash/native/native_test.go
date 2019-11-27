84cc4c4096eb3fc3961bba2cb8d7c40bfea63510
TAO <1659791130@qq.com>
2019-11-27 12:02:42 +0800

    update

--- consensus/qkchash/native/native_test.go
+++ consensus/qkchash/native/native_test.go
@@ -3,6 +3,7 @@ package native
 import (
 	"github.com/QuarkChain/goquarkchain/account"
 	"github.com/QuarkChain/goquarkchain/core"
+	"github.com/QuarkChain/goquarkchain/core/types"
 	"github.com/QuarkChain/goquarkchain/serialize"
 	"github.com/stretchr/testify/assert"
 	"math/big"
@@ -36,9 +37,46 @@ func TestNativeTokenTransfer(t *testing.T) {
 	assert.Equal(t, len(b1.Transactions()), 1)
 	shardState.FinalizeAndAddBlock(b1)
 	assert.Equal(t, shardState.CurrentHeader(), b1.Header())
-	tTxList, _, err := shardState.GetTransactionByAddress(acc1, &QETH, nil, 20)
+	tTxList, _, err := shardState.GetTransactionByAddress(acc1, nil, nil, 0)
 	if err != nil {
 		t.Errorf("GetTransactionByAddress error :%v", err)
 	}
 	assert.NotEqual(t, tTxList[0].Value, &serialize.Uint256{Value: big.NewInt(12345)})
+}
+func TestNativeTokenTransferValueSuccess(t *testing.T) {
+	MALICIOUS0 := common.TokenIDEncode("MALICIOUS0")
+	id1, _ := account.CreatRandomIdentity()
+	acc1 := account.CreatAddressFromIdentity(id1, 0)
+	acc3 := account.CreatEmptyAddress(0)
+	core.TestGenesisMinorTokenBalance["QKC"] = big.NewInt(10000000)
+	core.TestGenesisMinorTokenBalance["MALICIOUS0"] = big.NewInt(0)
+	env := core.GetTestEnv(&acc1, nil, nil, nil, nil, nil)
+	shardState := core.CreateDefaultShardState(env, nil, nil, nil, nil)
+	val := big.NewInt(0)
+	gas := uint64(21000)
+	gasPrice := uint64(1)
+	tx1 := core.CreateTransferTransaction(shardState, id1.GetKey().Bytes(), acc1, acc1, val, &gas, &gasPrice, nil, nil, nil, &MALICIOUS0)
+	error := shardState.AddTx(tx1)
+	if error != nil {
+		t.Errorf("addTx error: %v", error)
+	}
+	b1, _ := shardState.CreateBlockToMine(nil, &acc3, nil, nil, nil)
+	assert.Equal(t, len(b1.Transactions()), 1)
+	shardState.FinalizeAndAddBlock(b1)
+	assert.Equal(t, shardState.CurrentHeader(), b1.Header())
+	bl, _ := shardState.GetBalance(id1.GetRecipient(), nil)
+	QKC := common.TokenIDEncode("QKC")
+	assert.Equal(t, bl.GetTokenBalance(QKC), big.NewInt(10000000-21000))
+	b2, _ := shardState.GetBalance(acc1.Recipient, nil)
+	assert.Equal(t, b2.GetTokenBalance(MALICIOUS0), big.NewInt(0))
+	t1 := types.NewTokenBalancesWithMap(map[uint64]*big.Int{
+		MALICIOUS0: big.NewInt(0),
+		QKC:        big.NewInt(10000000 - 21000),
+	})
+	assert.NotEqual(t, b2, t1)
+	t2 := types.NewTokenBalancesWithMap(map[uint64]*big.Int{
+		QKC: big.NewInt(10000000 - 21000),
+	})
+	assert.Equal(t, b2, t2)
+
 }
